42ede8bc0c88dd4b193a66224b598c2833907e75
ctadev <53548168+ctadev@users.noreply.github.com>
2021-12-13 21:36:52 -0500

    second commit

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,12 +3,41 @@ import { useSelector } from "react-redux";
 
 function Song() {
   const songs = useSelector((state) => state.currentSongs);
+  const isPlaying = useSelector((state) => state.isPlaying);
 
   return (
     <div className="song-container">
-      <img src={songs.cover} alt={songs.name} />
+      <img
+        className={isPlaying ? "image play" : "image pause"}
+        src={songs.cover}
+        alt={songs.name}
+      />
       <h2>{songs.name}</h2>
       <p>{songs.artist}</p>
+      <div className="music-icon-animation">
+        <div>
+          <i
+            className={isPlaying ? "fas fa-music play" : "fas fa-music pause"}
+          ></i>
+          <i
+            className={isPlaying ? "fas fa-music play" : "fas fa-music pause"}
+          ></i>
+          <i
+            className={isPlaying ? "fas fa-music play" : "fas fa-music pause"}
+          ></i>
+        </div>
+        <div>
+          <i
+            className={isPlaying ? "fas fa-music play" : "fas fa-music pause"}
+          ></i>
+          <i
+            className={isPlaying ? "fas fa-music play" : "fas fa-music pause"}
+          ></i>
+          <i
+            className={isPlaying ? "fas fa-music play" : "fas fa-music pause"}
+          ></i>
+        </div>
+      </div>
     </div>
   );
 }
